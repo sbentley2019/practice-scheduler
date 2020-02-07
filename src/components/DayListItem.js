f14621b1b3c4567a60d6773606fb5427bf2a60fc
sbentley2019 <sbentley000@gmail.com>
2020-02-07 11:25:43 -0500

    Create button component conditions

---
+++ src/components/DayListItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  return (
+    <li selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
