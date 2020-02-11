d187aaae0da5411f9997572f95cd6401c14b24af
sbentley2019 <sbentley000@gmail.com>
2020-02-11 15:44:49 -0500

    start Empty component for appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
 
 export default function Appointment(props) {
   return <article className="appointment">{props.time}</article>;
