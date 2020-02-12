a331fa689df37d1a86b0db2497443788d5eecbd4
sbentley2019 <sbentley000@gmail.com>
2020-02-12 15:32:30 -0500

    add useEffect to application.js to axios to scheduler api for days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0
-  }
-];
 
 const appointments = [
   {
@@ -79,6 +61,13 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(res => {
+      setDays(res.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
