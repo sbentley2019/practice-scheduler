1ce16d27cb92580ad673e749696a1d2ad490710d
sbentley2019 <sbentley000@gmail.com>
2020-02-14 11:44:00 -0500

    add useVisualMode hook and have the appointment component conditioned on the mode value

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 import Header from "./Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => alert("onSave")}
+          onCancel={back}
+        />
       )}
     </article>
   );
