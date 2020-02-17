36e2c3461c66f02421b7d1bfdfaba392fff36eac
sbentley2019 <sbentley000@gmail.com>
2020-02-17 16:25:37 -0500

    create function to update spot counter in useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,40 +1,56 @@
-import { useState, useEffect, useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
 
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-const reducers = {
-  SET_DAY(state, action) {
-    return {
-      ...state,
-      day: action.day
-    };
-  },
-  SET_APPLICATION_DATA(state, action) {
-    return {
-      ...state,
-      days: action.days,
-      appointments: action.appointments,
-      interviewers: action.interviewers
-    };
-  },
-  SET_INTERVIEW(state, action) {
-    const appointment = {
-      ...state.appointments[action.id],
-      interview: action.interview ? { ...action.interview } : null
-    };
-    const appointments = { ...state.appointments, [action.id]: appointment };
-    return { ...state, appointments };
-  }
-};
-
-function reducer(state, action) {
-  return reducers[action.type](state, action) || state;
-}
-
 export default function useApplicationData() {
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+
+  const spotCounter = function(num, state, action) {
+    const day = {
+      ...state.days[num - 1],
+      spots: state.days[num - 1].spots + action.spotChange
+    };
+    const days = state.days.map(item => {
+      if (item.id === num) {
+        return day;
+      } else return item;
+    });
+
+    return days;
+  };
+
+  const reducers = {
+    SET_DAY(state, action) {
+      return {
+        ...state,
+        day: action.day
+      };
+    },
+    SET_APPLICATION_DATA(state, action) {
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      };
+    },
+    SET_INTERVIEW(state, action) {
+      const appointment = {
+        ...state.appointments[action.id],
+        interview: action.interview ? { ...action.interview } : null
+      };
+      const appointments = { ...state.appointments, [action.id]: appointment };
+      const days = spotCounter(Math.ceil(action.id / 5), state, action);
+
+      return { ...state, days, appointments };
+    }
+  };
+
+  function reducer(state, action) {
+    return reducers[action.type](state, action) || state;
+  }
+
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
@@ -65,13 +81,13 @@ export default function useApplicationData() {
 
   const bookInterview = function(id, interview) {
     return axios.put(`/api/appointments/${id}`, { interview }).then(data => {
-      dispatch({ type: SET_INTERVIEW, id, interview });
+      dispatch({ type: SET_INTERVIEW, id, interview, spotChange: -1 });
     });
   };
 
   const cancelInterview = function(id) {
     return axios.delete(`/api/appointments/${id}`).then(() => {
-      dispatch({ type: SET_INTERVIEW, id, interview: null });
+      dispatch({ type: SET_INTERVIEW, id, interview: null, spotChange: 1 });
     });
   };
 
